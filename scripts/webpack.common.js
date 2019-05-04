14a0a03b7e721b47df2889bab7f4dad8ae96bb23
Anani1994 <dongwhchn@163.com>
2019-05-04 23:58:23 +0800

    feat(*.less): 使用合适的 loader 在 JavaScript 模块中加载样式

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
+const rules = [{
+  test: /\.less$/,
+  use: [{
+    loader: 'style-loader'
+  }, {
+    loader: 'css-loader'
+  }, {
+    loader: 'less-loader'
+  }]
+}];
+
 const baseConfig = {
   entry: './app/index.js', // 入口文件，默认 main 作为名称
   output: {
     path: path.resolve(__dirname, '../dist'), // 指定输出文件所在目录
     filename: '[name].js' // 输出文件名，其中 name 为变量，值为入口文件名
+  },
+  module: {
+    rules: rules
   }
 };
 

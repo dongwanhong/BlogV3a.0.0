a3f200b1b5e7466edc40645ae75b224eb5da5e83
Anani1994 <dongwhchn@163.com>
2019-05-04 15:47:46 +0800

    feat(*): 使用脚本命令启用基本配置进行打包

---
+++ package-scripts.js
@@ -0,0 +1,19 @@
+// See https://github.com/kentcdodds/nps for more about nps.
+// See https://doc.esdoc.org/github.com/kentcdodds/nps-utils/ for more about nps-utils.
+
+const npsUtils = require('nps-utils');
+const { series, rimraf }  = npsUtils;
+
+module.exports = {
+  scripts: {
+    build: {
+      default: {
+        description: 'Clean dist directory and run all builds',
+        script: series(
+          rimraf('dist/*'),
+          'webpack --config config/webpack.common.js'
+        )
+      }
+    }
+  }
+};

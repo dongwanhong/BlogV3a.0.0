242216dc3d65130ffbbf575a16f15042bbf1ff4b
Anani1994 <dongwhchn@163.com>
2019-05-06 21:39:13 +0800

    feat(*): 使用 TerserJSPlugin 和 OptimizeCSSAssetsPlugin 压缩代码

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,8 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 const prodConfig = {
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new TerserJSPlugin()
+    ]
+  }
 };
 
 module.exports = merge(common, prodConfig);

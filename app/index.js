959ac079f70d2cf1b739c6107d654e65160c0c85
Anani1994 <dongwhchn@163.com>
2019-05-09 21:58:29 +0800

    feat(*): 使用 source map 定位在原文件中的位置

--- app/index.js
+++ app/index.js
@@ -1,9 +1,3 @@
 import './styles/app.less'
 
-import Icon from './images/doraemon.png';
-
-const myIcon = new Image();
-myIcon.src = Icon;
-myIcon.width = 150;
-myIcon.height = 150;
-document.body.appendChild(myIcon);
+console.log(undefinedVariable);
--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -2,7 +2,8 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
 const devConfig = {
-  mode: 'development'
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
 };
 
 module.exports = merge(common, devConfig);

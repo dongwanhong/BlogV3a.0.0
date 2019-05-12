8e68e762897737c578999e688776d1b043941456
Anani1994 <dongwhchn@163.com>
2019-05-12 11:19:19 +0800

    feat(*): 使用 purifycss-webpack 消除未生效的样式代码

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
+const glob = require('glob-all');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 const prodConfig = {
   mode: 'production',
@@ -10,7 +13,16 @@ const prodConfig = {
       new OptimizeCSSAssetsPlugin(),
       new TerserJSPlugin()
     ]
-  }
+  },
+  plugins: [
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute
+      paths: glob.sync([
+        path.join(process.cwd(), 'app/**/*.html'),
+        path.join(process.cwd(), 'app/**/*.js'),
+      ]),
+    }),
+  ],
 };
 
 module.exports = merge(common, prodConfig);

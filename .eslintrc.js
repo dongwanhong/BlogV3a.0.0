0f569913f67c024bbcf055913411356df4725fd1
Anani1994 <dongwhchn@163.com>
2019-05-19 11:48:42 +0800

    feat(*): 使用相应包解决 lint 与 prettier 之间的冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -12,5 +12,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  rules: {}
+  rules: {
+    'prettier/prettier': 'error'
+  }
 }
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   processors: [],
   plugins: [],
-  extends: 'stylelint-config-standard',
+  extends: [
+    // 继承的规则
+    'stylelint-config-standard',
+    'stylelint-config-prettier'
+  ],
   rules: {
+    // 指定特定规则
+    'rule-empty-line-before': [
+      'always',
+      {
+        severity: 'error'
+      }
+    ]
   }
 }

9aa73a4354e1953cb999aa7d7cebceea5001d5ed
Anani1994 <dongwhchn@163.com>
2019-07-15 22:19:11 +0800

    feat(*): 封装高阶组件 RouteAnimated 实现路由切换动画

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { hot } from 'react-hot-loader/root'
@@ -7,13 +7,14 @@ import Home from './Home'
 import Main from './Main'
 
 @hot
-class App extends Component {
+class App extends PureComponent {
   public render(): React.ReactNode {
     return (
       <Provider store={store}>
         <Router>
-          <Route path="/" exact component={Home} />
-          <Route path="/main/" exact component={Main} />
+          {/* Do not use `Switch` component to avoid failing to implement exit animation */}
+          <Route path="/" exact children={props => <Home {...props} />} />
+          <Route path="/main" children={props => <Main {...props} />} />
         </Router>
       </Provider>
     )

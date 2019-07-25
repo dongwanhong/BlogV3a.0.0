64c8073e51a581f0a3626e212868e53fcc293cc0
Anani1994 <dongwhchn@163.com>
2019-07-25 23:03:10 +0800

    feat(step*): 调整和新增步骤组件的条形样式

--- src/components/common/Step/components/StepItem.tsx
+++ src/components/common/Step/components/StepItem.tsx
@@ -1,3 +1,10 @@
+/**
+ * @description
+ * 指定每条步骤的提示信息
+ * @param {string} title 可选的，标题
+ * @param {string} description 可选的，对步骤的详情描述
+ */
+
 import React, { Component, ReactNode } from 'react'
 import { setActiveStepType } from '../index'
 
@@ -6,16 +13,30 @@ interface Props {
   description?: string
   index?: number
   activeIndex?: number
+  extra?: boolean
   setActiveStep?: setActiveStepType
 }
 
 class StepItem extends Component<Props, {}> {
   public render(): ReactNode {
-    const { title, description, index = 0, activeIndex = 0, setActiveStep = () => {} } = this.props
+    const {
+      title,
+      description,
+      index = 0,
+      activeIndex = 0,
+      extra,
+      setActiveStep = () => {}
+    } = this.props
     let classNames = 'step-item'
 
-    if (index === activeIndex && !classNames.includes('active')) {
-      classNames += ' active'
+    if (index === activeIndex) {
+      if (extra) {
+        classNames.replace(/done/, '')
+        !classNames.includes('active') && (classNames += ' active')
+      } else {
+        classNames.replace(/active/, '')
+        !classNames.includes('done') && (classNames += ' done')
+      }
     }
 
     return (

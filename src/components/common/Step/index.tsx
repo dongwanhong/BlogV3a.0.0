435d17e58874bfec0fb52c2a84d35f5d59076317
Anani1994 <dongwhchn@163.com>
2019-07-25 23:03:10 +0800

    feat(step*): 调整和新增步骤组件的条形样式

--- src/components/common/Step/index.tsx
+++ src/components/common/Step/index.tsx
@@ -1,15 +1,27 @@
+/**
+ * @description
+ * 传递可选参数控制步骤组件的显示样式
+ * @param {'circle' | 'strip'} mode 可选的，步骤条的显示样式，默认为 'circle' 圆点显示
+ * @param {'vertical' | 'horizontal'} direction 可选的，设置步骤条的显示方向，默认为 ’horizontal‘ 水平放置
+ * @param {'color' | 'number' | 'all'} content 可选的，在 'circle' 模式，指定圆圈中以数字或颜色填充，默认为数字
+ * @param {boolean} line 可选的，是否添加横线，只在 'circle' 模式下的水平布局下的非单点高亮有效
+ * @param {boolean} extra 可选的，配合在 content 为 'color' 的模式下进行单点高亮
+ */
+
 import React, { PureComponent, ReactNode } from 'react'
 import StepItem from './components/StepItem'
 
-type mode = 'line'
-type content = 'color' | 'number'
-type direction = 'vertical' | 'horizontal'
+type modeType = 'circle' | 'strip'
+type contentType = 'color' | 'number' | 'all'
+type directionType = 'vertical' | 'horizontal'
 export type setActiveStepType = (index: number) => void
 
 interface Props {
-  mode?: mode
-  direction?: direction
-  content?: content
+  mode?: modeType
+  direction?: directionType
+  content?: contentType
+  line?: boolean
+  extra?: boolean
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onChange?: (index: number) => any
 }
@@ -50,9 +62,23 @@ class Step extends PureComponent<Props, State> {
 
   public render(): ReactNode {
     const { setActiveStep } = this
-    const { children, mode, content = 'number', direction = 'horizontal' } = this.props
+    const { children, mode = 'circle', content, direction = 'horizontal', line, extra } = this.props
     const { activeIndex } = this.state
-    const classNames = [mode, content, direction, 'step-wrapper']
+    const classNames: (number | string | undefined)[] = [mode, direction, 'step-wrapper']
+
+    if (content === 'all') {
+      classNames.concat(['color', 'number'])
+    } else {
+      classNames.push(content)
+    }
+
+    if (mode === 'circle' && direction === 'horizontal' && line && !extra) {
+      classNames.push('line')
+    }
+
+    if (extra) {
+      classNames.push('dot')
+    }
 
     return (
       <div
@@ -63,7 +89,7 @@ class Step extends PureComponent<Props, State> {
       >
         {React.Children.map(children, (oChildren, index) =>
           React.isValidElement(oChildren)
-            ? React.cloneElement(oChildren, { index, activeIndex, setActiveStep })
+            ? React.cloneElement(oChildren, { index, activeIndex, setActiveStep, extra })
             : null
         )}
       </div>
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,14 +1,17 @@
 import React, { PureComponent } from 'react'
-import { PageScroll } from '../../components'
+import { Step } from '../../components'
 import { withAnimateRoute } from '../../components'
 
 class Main extends PureComponent {
   public render(): React.ReactNode {
     return (
-      <PageScroll>
-        <PageScroll.PageItem>1</PageScroll.PageItem>
-        <PageScroll.PageItem>2</PageScroll.PageItem>
-      </PageScroll>
+      <Step>
+        <Step.StepItem title="第一步" />
+        <Step.StepItem title="第二步" />
+        <Step.StepItem title="第三步" description="我是第三步的描述" />
+        <Step.StepItem title="第四步" />
+        <Step.StepItem title="第五步" />
+      </Step>
     )
   }
 }

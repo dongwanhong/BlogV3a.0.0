0971410f0879b76963f9aa569eb50599b1dd1091
Anani1994 <dongwhchn@163.com>
2019-07-27 23:54:45 +0800

    feat(page scroll): 使用 Props 管理全屏滚动组件以避免使用派生 State

--- src/components/common/PageScroll/index.tsx
+++ src/components/common/PageScroll/index.tsx
@@ -1,59 +1,70 @@
+/**
+ * @description
+ * 全屏滚动组件
+ * @param {number} activeIndex 可选的，当前显示的屏数，以零开始计
+ * @param {(isDown: boolean) => void} onSlide 可选的，滑动时执行的函数，参数为是否是下滑
+ */
+
 import React, { PureComponent, ReactNode, WheelEvent } from 'react'
 import utils from '../../../utils'
 import PageItem from './components/PageItem'
 
-interface State {
-  top: number
-  height: number
+interface Props {
+  activeIndex?: number
+  onSlide?: (isDown: boolean) => void
 }
 
-class PageScroll extends PureComponent<{}, State> {
+class PageScroll extends PureComponent<Props, {}> {
   public static PageItem = PageItem
   public totalHeight: number
+  public count: number
+  public height: number
 
-  public state = {
-    top: 0,
-    height: document.documentElement.clientHeight || document.body.clientHeight
-  }
-
-  public constructor(props: {}) {
+  public constructor(props: Props) {
     super(props)
+    this.count = 0
+    this.height = 0
     this.totalHeight = 0
     this.handleWheel = utils.throttle(this.handleWheel.bind(this), 1000)
+    this.getTop = this.getTop.bind(this)
   }
 
-  public componentDidMount(): void {
-    const { height } = this.state
+  public componentWillMount(): void {
     const { children } = this.props
-    const childrenLen = React.Children.count(children)
-    this.totalHeight = height * childrenLen
+    this.count = React.Children.count(children)
+    this.height = document.documentElement.clientHeight || document.body.clientHeight
+    this.totalHeight = this.height * this.count
   }
 
   public handleWheel(event: WheelEvent<HTMLDivElement>): void {
     const { deltaY } = event
-    const { totalHeight } = this
-    const { top, height } = this.state
-    if (deltaY < 0 && top < 0) {
-      this.setState(() => ({
-        top: top + height
-      }))
-    } else if (deltaY > 0 && top > height - totalHeight) {
-      this.setState(() => ({
-        top: top - height
-      }))
+    const { onSlide } = this.props
+    if (onSlide) {
+      onSlide(deltaY < 0)
+    }
+  }
+
+  public getTop(): number {
+    const { activeIndex = 0 } = this.props
+    const { count, height, totalHeight } = this
+    if (activeIndex < 0) {
+      return 0
+    } else if (activeIndex + 1 > count) {
+      return height - totalHeight
+    } else {
+      return -activeIndex * height
     }
   }
 
   public render(): ReactNode {
-    const { totalHeight, handleWheel } = this
-    const { height, top } = this.state
+    const { height, totalHeight, getTop, handleWheel } = this
     const { children } = this.props
 
     return (
       <div className="page-scroll" style={{ height }}>
         <div
           className="scroll-container"
-          style={{ height: totalHeight, top }}
+          style={{ height: totalHeight, top: getTop() }}
           onWheel={handleWheel}
         >
           {React.Children.map(children, oChildren =>

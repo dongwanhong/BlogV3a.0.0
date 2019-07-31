348a31ff2684670c129cd56a0825726592acdb97
Anani1994 <dongwhchn@163.com>
2019-07-31 21:18:10 +0800

    feat(water-wave*): 添加 WaterWave 水波组件

---
+++ src/components/common/WateWave/index.tsx
@@ -0,0 +1,60 @@
+/**
+ * @description
+ * 在包括的元素内点击产生水波效果
+ * @param {string} url 背景图片地址
+ */
+
+import React, { PureComponent, ReactChild, createRef, MouseEvent, RefObject } from 'react'
+
+interface Props {
+  url: string
+}
+
+class WaterWave extends PureComponent<Props, {}> {
+  public eleRef: RefObject<HTMLDivElement>
+
+  public constructor(props: Props) {
+    super(props)
+    this.eleRef = createRef()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  public handleClick(eve: MouseEvent): void {
+    const { eleRef } = this
+    if (!eleRef.current) return
+    const top = eve.pageY - 80
+    const left = eve.pageX - 80
+    const { url } = this.props
+    let children: HTMLDivElement
+    const waveWrapper = document.createElement('div')
+    waveWrapper.classList.add('wave-wrapper')
+    waveWrapper.style.top = `${top}px`
+    waveWrapper.style.left = `${left}px`
+    for (let i = 0; i < 5; i++) {
+      children = document.createElement('div')
+      children.classList.add('wave')
+      children.style.backgroundImage = `url(${url})`
+      waveWrapper.appendChild(children)
+    }
+    eleRef.current.appendChild(waveWrapper)
+    setTimeout(() => eleRef.current && eleRef.current.removeChild(waveWrapper), 1600)
+  }
+
+  public render(): ReactChild {
+    const { eleRef, handleClick } = this
+    const { children, url } = this.props
+
+    return (
+      <div
+        ref={eleRef}
+        className="wave-container"
+        style={{ backgroundImage: `url(${url})` }}
+        onClick={eve => handleClick(eve)}
+      >
+        {children}
+      </div>
+    )
+  }
+}
+
+export default WaterWave
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,8 +5,9 @@ import withAnimateRoute from './hoc/RouteAnimated'
 // Common
 import { PageScroll, PageItem } from './common/PageScroll'
 import { Step, StepItem } from './common/Step'
+import WaterWave from './common/WateWave'
 // Custom
 import TopBar from './custom/TopBar'
 import Footer from './custom/Footer'
 
-export { Link, withAnimateRoute, PageScroll, PageItem, Step, StepItem, TopBar, Footer }
+export { Link, withAnimateRoute, PageScroll, PageItem, Step, StepItem, WaterWave, TopBar, Footer }

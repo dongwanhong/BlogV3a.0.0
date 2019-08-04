1e4e12c417ce5f741c7b94f9e2a8fbda30a5cc2f
Anani1994 <dongwhchn@163.com>
2019-08-04 17:53:03 +0800

    feat(util): 完成工具雨类的实现

--- src/utils/rain/types.ts
+++ src/utils/rain/types.ts
@@ -7,17 +7,32 @@ export interface Config {
   color?: string
   skyColor?: string
   count?: number
+  tally?: number
+}
+
+export interface WaterDrop {
+  x: number
+  y: number
+  angle: number
+  length: number
+  vy: number
+  boom: boolean
+  die: boolean
+}
+
+export interface Droplet {
+  oriX: number
+  oriY: number
+  currentX: number
+  currentY: number
+  angle: number
+  r: number
+  R: number
+  vy: number
+  isX: boolean
+  die: boolean
 }
 
 export type DefaultConfig = Required<Omit<Config, 'ele'>>
 
 export type ResultConfig = Config & DefaultConfig
-
-export interface WaterDrop {
-  x: number
-  y: number
-  vx: number
-  vy: number
-  length: number
-  die: boolean
-}

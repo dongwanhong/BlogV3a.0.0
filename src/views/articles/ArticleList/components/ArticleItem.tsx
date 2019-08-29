c6c1b362d2217670b0674ea03a57b08896b613c8
Anani1994 <dongwhchn@163.com>
2019-08-29 22:39:45 +0800

    feat(article-list*): 调整样式适配移动端的布局

--- src/views/articles/ArticleList/components/ArticleItem.tsx
+++ src/views/articles/ArticleList/components/ArticleItem.tsx
@@ -4,24 +4,29 @@ import { Link } from '@/components'
 interface Props {
   des: string
   src: string
+  alt?: string
   to: string
   title: string
 }
 
 const ArticleItem: SFC<Props> = props => {
-  const { to, des, src, title } = props
+  const { to, des, src, title, alt } = props
 
   return (
-    <div className="article-item">
-      <div className="article-title">{title}</div>
-      <div className="article-content">
-        <div className="article-left">
-          <img src={src} alt="mias" />
+    <div className="article-item container-fluid">
+      <div className="article-title row">
+        <div className="col-lg-12">
+          <Link to={to}>{title}</Link>
         </div>
-        <div className="article-right">
+      </div>
+      <div className="article-content row">
+        <div className="article-left col-lg-4 col-xs-12">
+          <img src={src} alt={alt || ''} />
+        </div>
+        <div className="article-right col-lg-8 col-xs-12">
           <div className="short-des">{`摘要：${des}`}</div>
           <div className="more-btn">
-            <Link to={to}>查看详情 ></Link>
+            <Link to={to}>查看详情 &gt;</Link>
           </div>
         </div>
       </div>

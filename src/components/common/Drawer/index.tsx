707ddd6dfc25c1c3fb24f379b2f78616f81322c0
Anani1994 <dongwhchn@163.com>
2019-11-25 20:39:49 +0800

    feat(drawer*): 改进抽屉组件的样式和动画效果

--- src/components/common/Drawer/index.tsx
+++ src/components/common/Drawer/index.tsx
@@ -1,12 +1,25 @@
-import React, { SFC } from 'react'
+/**
+ * 由于遮罩层的宽度较大（一个屏幕），所以整体滑动不太友好
+ * 所以直接滑动内容板块
+ * 进一步将遮罩层从容器上分离出来添加透明度的切换动画
+ */
+import React, { SFC, RefObject, createRef } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { CSSTransitionClassNames } from 'react-transition-group/CSSTransition'
 
 const classNames: CSSTransitionClassNames = {
   enter: 'animated',
-  enterActive: 'slideInLeft',
+  enterActive: 'slideInRight',
+  enterDone: 'open',
   exit: 'animated',
-  exitActive: 'slideOutLeft'
+  exitActive: 'slideOutRight'
+}
+
+const maskClassNames: CSSTransitionClassNames = {
+  enter: 'animated',
+  enterActive: 'fadeIn',
+  exit: 'animated',
+  exitActive: 'fadeOut'
 }
 
 interface Props {
@@ -16,17 +29,29 @@ interface Props {
   onClose: () => void
 }
 
-const handleMaskClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, cb: () => void): void => {
-  if (e.target === e.currentTarget) {
+const handleMaskClick = (
+  e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  maskEle: RefObject<HTMLDivElement>,
+  cb: () => void
+): void => {
+  if (maskEle.current === e.target) {
     cb()
   }
 }
 
 const Draweer: SFC<Props> = props => {
+  const maskEle: RefObject<HTMLDivElement> = createRef()
   const { show = true, children, title = '', footnote = '', onClose = () => {} } = props
   return (
-    <CSSTransition in={show} timeout={1000} classNames={classNames} unmountOnExit={true}>
-      <div className="drawer-container" onClick={e => handleMaskClick(e, onClose)}>
+    <div
+      className="drawer-container"
+      style={{ width: show ? '100%' : '' }}
+      onClick={e => handleMaskClick(e, maskEle, onClose)}
+    >
+      <CSSTransition in={show} timeout={1000} classNames={maskClassNames} unmountOnExit={false}>
+        <div className="drawer-mask" ref={maskEle} />
+      </CSSTransition>
+      <CSSTransition in={show} timeout={1000} classNames={classNames} unmountOnExit={false}>
         <div className="drawer-content">
           {title ? <div className="drawer-header">{title}</div> : null}
           <div className="drawer-body" style={footnote ? {} : { height: '100%' }}>
@@ -34,8 +59,8 @@ const Draweer: SFC<Props> = props => {
           </div>
           {footnote ? <div className="drawer-footer">{footnote}</div> : null}
         </div>
-      </div>
-    </CSSTransition>
+      </CSSTransition>
+    </div>
   )
 }
 

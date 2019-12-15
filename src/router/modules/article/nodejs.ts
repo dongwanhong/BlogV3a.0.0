3b8188595246349f019797dca936a2d5902eb877
Anani1994 <dongwhchn@163.com>
2019-12-15 21:13:38 +0800

    fix(src/): 微博图床限制外链导致图片无法显示

--- src/router/modules/article/nodejs.ts
+++ src/router/modules/article/nodejs.ts
@@ -1,4 +1,5 @@
 import { lazy } from 'react'
+import contentImage from '@/images/pages/articles/node-require.png'
 
 const nodejs = [
   {
@@ -13,7 +14,7 @@ const nodejs = [
     title: '理清 Nodejs 中的 require 函数',
     des:
       '在 ES6 出现之前 JavaScript 本身并没有模块的概念，不支持封闭的作用域和依赖管理，这对于开发大型项目来说十分头疼。所以，社区制定了一些模块加载方案来解决这个问题，其中就包...',
-    src: 'https://ws1.sinaimg.cn/large/dac84c10ly1g6vyid8uwtj21xgb38qv7.jpg',
+    src: contentImage,
     tags: ['JavaScript', 'Node.js'],
     types: ['后端开发']
   }
